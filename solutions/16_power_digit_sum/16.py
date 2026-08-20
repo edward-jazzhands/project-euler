0efc81500f2290dba0d7adde17179e7e09d167f7
Edward Jazzhands <ed.jazzhands@gmail.com>
2026-08-20 14:37:31 -0700

    finished puzzle 16

--- solutions/16_power_digit_sum/16.py
+++ solutions/16_power_digit_sum/16.py
@@ -3,3 +3,16 @@
 What is the sum of the digits of the number 2^{1000}?
 
 """
+
+x = 2 ** 1000
+strx = str(x)
+list_strx = list(strx)
+list_intx = [int(x) for x in list_strx]
+ans = sum(list_intx)
+print(ans)
+
+# code golfing:
+print(sum([int(x) for x in list(str(2 ** 1000))]))
+
+# should be 1366
+
