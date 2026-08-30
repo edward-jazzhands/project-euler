1864309468330767b2ece6dbcdd9040c51e23430
Edward Jazzhands <ed.jazzhands@gmail.com>
2026-08-30 06:41:26 -0700

    solved 23

--- solutions/23_non-abundant_sums/23.py
+++ solutions/23_non-abundant_sums/23.py
@@ -14,3 +14,67 @@ this limit.
 Find the sum of all the positive integers which cannot be written as the sum of 
 two abundant numbers.
 """
+
+# the greatest number that cannot be expressed as the sum of two abundant numbers
+# is less than 28123
+
+import math
+import time
+
+def get_factors(n: int) -> list:
+    factors = [1]     # 1 is always a factor
+    isqrt = math.isqrt(n)   # use isqrt instead of sqrt for looping
+    for i in range(2, isqrt+1):
+        if n % i == 0:
+            if i == n // i:  # this means i is the square root of n
+                factors.append(i)
+            else:
+                factors.append(i)
+                factors.append(n // i)
+                
+    factors.sort()
+    return factors
+
+def is_abundant(n: int) -> bool:
+
+    factors = get_factors(n)
+    f_sum = sum(factors)
+    if f_sum > n:
+        return True
+    else:
+        return False
+
+# Find the sum of all the positive integers which 
+# cannot be expressed as the sum of two abundant numbers.
+
+# we know the greatest number that cannot be expressed as the sum of two abundant numbers
+# is less than 28123
+# which means when finding the abundant numbers to check against, we only need to collect
+# up to 28123.
+
+start = time.time()
+
+abundants = []
+for n in range(1, 28123):
+    if is_abundant(n):
+        abundants.append(n)
+
+# pre-load the entire cache - takes about 5 seconds
+print("starting cache pre-load")
+abundant_sums: set[int] = set()
+for x in abundants:
+    for y in abundants:
+        z = x + y
+        if z not in abundant_sums:
+            abundant_sums.add(z)
+
+print("finished cache pre-load")
+
+total = 0
+for n in range(1, 28123):
+    if n not in abundant_sums:
+        total += n
+
+print(total)
+print(f"Total script time:  {time.time() - start:.2f}")
+
