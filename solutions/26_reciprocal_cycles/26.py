3d6d190870159d4ba5cbc9e7c75732d86d89f234
Edward Jazzhands <ed.jazzhands@gmail.com>
2026-09-08 00:59:14 -0700

    optimized 26 + 27

--- solutions/26_reciprocal_cycles/26.py
+++ solutions/26_reciprocal_cycles/26.py
@@ -20,6 +20,10 @@ in its decimal fraction part.
 """
 
 from decimal import Decimal, getcontext
+import time
+
+
+# VERSION 1: My original version
 
 # NOTE: 2000 is the minimum precision to get the correct answer in this puzzle
 getcontext().prec = 2000
@@ -51,6 +55,8 @@ def find_repeat(s: str) -> str | None:
 
     return None
 
+start1 = time.time()
+
 longest = ""
 iteration = 0
 
@@ -66,5 +72,94 @@ for i in range(2, 1000):
             # print(f"\033[34m new longest pattern for {i}: \033[0m {len(repeats)}")
             iteration = i
 
+end1 = time.time() - start1
+print(f"Time taken: {end1:.5f}")
+
 print(f"\nAnswer: {iteration = }, had length: {len(longest)}")
     
+
+# -----------------------------------------------------
+
+# VERSION 2: Better solution (copied):
+
+# This version uses a cool math trick, which is very simple once you understand
+# how it works. It is significantly faster, roughly from O(n^2) down to O(n)
+
+def recurring_cycle_length(d: int) -> int:
+    remainder = 1
+    seen = {}
+
+    position = 0
+
+    while remainder:  # stops if remainder = 0
+        if remainder in seen:
+            # the length of the pattern will be the current position minus the
+            # last time this remainder was seen
+            return position - seen[remainder]
+
+        seen[remainder] = position
+
+        remainder *= 10
+        remainder %= d
+        position += 1
+
+    # loop only stops if remainder is 0, so just return 0
+    return 0
+
+    # NOTE: This is literally doing long division but expressed as python
+    # Using d=7 for example:
+    # position 0:  remainder=(1*10)=10 | remainder % d == 3  (10-7=3)  - decimal 1
+    # position 1:  remainder=(3*10)=30 | remainder % d == 2  (30-28=2) - decimal 4
+    # position 2:  remainder=(2*10)=20 | remainder % d == 6  (20-14=6) - decimal 2
+    # position 3:  remainder=(6*10)=60 | remainder % d == 4  (60-56=4) - decimal 8
+    # position 4:  remainder=(4*10)=40 | remainder % d == 5  (40-35=5) - decimal 5
+    # position 5:  remainder=(5*10)=50 | remainder % d == 1  (50-49=1) - decimal 7
+    # position 6:  remainder=(1*10)=10 | remainder % d == 3  (10-7=3)  - decimal 1
+
+    # In long-hand to make it extra clear:
+
+    #       0.1428571
+    #      -----------------
+    #   7 / 1 0000000
+    #         7
+    #       ----
+    #         30
+    #         28
+    #       ----
+    #          20
+    #          14
+    #         ----
+    #           60
+    #           56
+    #          ----
+    #            40
+    #            35
+    #           ----
+    #             50
+    #             49
+    #            ----
+    #              10
+    #               7
+
+    # This works because using long division, once we see the same number again,
+    # we know mathematatically it must repeat the same pattern from that point.
+    # This means we only need to track remainders we have seen, and the pattern is found
+    # as soon as we see any remainder twice.
+    # This greatly reduces the amount of calculations needed compared to
+    # the first version, it brings it from O(n^2) down to O(n).
+
+start2 = time.time()##
+
+longest = 0
+answer = 0
+
+for d in range(2, 1000):
+    length = recurring_cycle_length(d)
+
+    if length > longest:
+        longest = length
+        answer = d
+
+
+end2 = time.time() - start2
+print(f"Time taken: {end2:.5f}")
--- solutions/27_quadratic_primes/27.py
+++ solutions/27_quadratic_primes/27.py
@@ -22,10 +22,17 @@ starting with n = 0.
 """
 
 import math
+import time
+# import sys
 
 def prime_checker(n: int) -> bool:
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+        
     # this check would be unnecessary if we're only inputting
-    # odd numbers into the function
+    # odd numbers into the function:
     if n % 2 == 0:
         return False
 
@@ -36,39 +43,68 @@ def prime_checker(n: int) -> bool:
             return False
     return True
 
-assert prime_checker(25) is False
-assert prime_checker(33) is False
-assert prime_checker(17) is True
-assert prime_checker(31) is True
 
+# puzzle says |a| < 1000 and |b| <= 1000
+# that means a is between -999 and 999
+# while b is between -1000 and 1000
 
-most_primes = []
+# HOWEVER, we don't actually need to start b at -1000.
+# Because it always starts each look with n=0, when that is plugged into
+# the equation, you'll get: n**0 + a*0 + b, which of course is just 'b'.
+
+# So if b is not prime, it would break after 1 loop.
+# And since we know the first prime number is 2, this means logically
+# that b cannot be less than 2. It also must be odd (all primes are odd).
+
+# We ALSO know that a must be odd as well! This is because at n=1,
+# you will get: n**1 (which is 1) + a*1 (which is a) + b
+# this simplifies to `1 + a + b`.
+# We already know b must be odd, and 1 + any odd will make an even number.
+# Logically this means 1 must also be odd, to flip the result back to being odd.
+# if not, it will fail on the second loop. So we can skip those as well.
+
+start = time.time()
+
+most_primes = 0
 coefficients = (0, 0)
 
-for i in range(-1000, 1001):
-    for j in range(-1000, 1001):
+# pre-generate the primes list for the b coefficient
+prime_Bs = [2] + [b for b in range(3, 1001, 2) if prime_checker(b)]
 
-        primes = []
+for a in range(-999, 1000, 2):
+    for b in prime_Bs:
+
+        primes = 0
+
         n = 0
         # loop until we hit one that is not prime
         while True:
-            # print(f"z = {n}**2 + ({i}*{n}) + {j}")
-            z = n**2 + (i*n) + j
+            # print(f"z = {n}**2 + ({i}*{n}) + {j}")   # enable for debug
+
+            # while loop here is pretty simple - plug a and b into the equation,
+            # check if the result is a prime number. Keep going until it
+            # hits one that is not prime. Then check if we have a new winner.
+
+            z = n**2 + (a*n) + b
+
             if z <= 0:  # can't be prime if less than 0
                 break
             is_prime = prime_checker(z)
             if is_prime:
-                primes.append(z)
+                primes += 1
             else:
                 break
             n += 1
         
-        if len(primes) > len(most_primes):
+        if primes > most_primes:
             most_primes = primes
-            coefficients = (i, j)
-            print(f"New winning coefficients:  {coefficients} | {len(most_primes) = }")
+            coefficients = (a, b)
+            # print(f"New winning coefficients:  {coefficients} | {len(most_primes) = }")
             
 print()
 print(f"Equation:  n**2 + ({coefficients[0]}*n) + {coefficients[1]}")
 print(f"{coefficients = }")
 print(f"Answer: {coefficients[0] * coefficients[1]}")
+
+end = time.time() - start
+print(f"Time taken: {end:.5f}")
